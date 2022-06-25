ce6162e21006935b64c8be588d9ec19241435528
naima1515 <mirnanina2014@gmail.com>
2022-06-25 20:44:48 +0100

    modify indexJs

--- index.js
+++ index.js
@@ -9,43 +9,50 @@ const errorPage = fs.readFileSync('./404.html');
 const styles = fs.readFileSync('./styles.css');
 const stylesJs = fs.readFileSync('./uiScript.js');
 app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(homePage);
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(homePage);
+  // res.end();
+  res.end(homePage);
 });
 app.get('/index.html', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(homePage);
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(homePage);
+  // res.end();
+  res.end(homePage);
 });
 
 app.get('/about.html', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(aboutPage);
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(aboutPage);
+  // res.end();
+  res.end(aboutPage);
 });
 
 app.get('/contact.html', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(contactPage);
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(contactPage);
+  // res.end();
+  res.end(contactPage);
 });
 
 app.get('/styles.css', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/css' });
-  res.write(styles);
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/css' });
+  // res.write(styles);
+  // res.end();
+  res.end(styles);
 });
 
 app.get('/uiScript.js', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/js' });
-  res.write(stylesJs);
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/js' });
+  // res.write(stylesJs);
+  // res.end();
+  res.end(stylesJs);
 });
 
 app.get('*', (req, res) => {
-  res.writeHead(404, { 'Content-Type': 'text/html' });
-  res.write(errorPage);
-  res.end();
+  // res.writeHead(404, { 'Content-Type': 'text/html' });
+  // res.write(errorPage);
+  // res.end();
+  res.end(errorPage);
 });
 app.listen(port, () => console.log('app running...'));
